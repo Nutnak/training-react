a7daaaf40de722c37069ebf4ca5a75706be883be
Quentin Doulcet <quentinucme@hotmail.fr>
2023-06-17 16:07:54 -0400

    first component

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+      <div className="home">
+          <h1>Ma progression</h1>
+          <div>Bienvenue sur ce site qui est dédié à ma progression en React</div>
+      </div>
+  )
+}
