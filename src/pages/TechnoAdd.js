b10223bda4fa6cd1b0d3bb79275d0d741a5a8428
Quentin Doulcet <quentinucme@hotmail.fr>
2023-06-17 17:18:19 -0400

    last component

--- src/pages/TechnoAdd.js
+++ src/pages/TechnoAdd.js
@@ -1,5 +1,5 @@
 export default function TechnoAdd() {
-  return(
+  return (
     <div className="techno-add">
       <h1>Add a techno</h1>
       <div>
@@ -8,7 +8,21 @@ export default function TechnoAdd() {
           <br />
           <input type="text" name="" id="techno-name"></input>
           <br />
-          <input type="submit" value ="Add Techno" />
+          <label htmlFor="techno-category">Category:</label>
+          <br />
+          <select name="techno-category" id="techno-category">
+            <option value="">Selectionnez une techno</option>
+            <option value="front">Front</option>
+            <option value="back">Back</option>
+            <option value="fullstack">Fullstack</option>
+            <option value="other">other</option>
+          </select>
+          <br />
+          <label htmlFor="techno-description">Description:</label>
+          <br />
+          <textarea name="techno-descrition" id="techno-description" col="30" row="10"></textarea>
+          <br />
+          <input type="submit" value="Add Techno"/>
         </form>
       </div>
 
